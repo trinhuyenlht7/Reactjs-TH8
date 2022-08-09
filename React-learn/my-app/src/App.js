492f839eaa2b557c7b2303e6ccd70001de4c98e4
trinh_uyen <trinhuyenlht7@gmail.com>
2022-08-09 20:26:53 +0700

    thay doi lan 2

---
+++ React-learn/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/kid.trinh.3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Trinh Uyen dep trai hihi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
